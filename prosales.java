8d18c847e835bccb9eda8ada06dc72698656f865
Ubuntu <ubuntu@ip-172-31-11-78.ap-south-1.compute.internal>
2025-02-22 04:54:39 +0000

    4th Commit

--- prosales.java
+++ prosales.java
@@ -4,3 +4,6 @@ DevOps
 let carName1 = "Volvo XC60";  // Double quotes
 let carName2 = 'Volvo XC60';  // Single quotes
 I'm From Dev Branch
+let answer1 = "It's alright";
+let answer2 = "He is called 'Johnny'";
+let answer3 = 'He is called "Johnny"';
